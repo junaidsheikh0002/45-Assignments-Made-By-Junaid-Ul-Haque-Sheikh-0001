c55fcb50838461600b0aa1ffab88f95424940616
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-06-15 07:09:37 +0500

    Update Q1,2,3.js

--- Q1,2,3.js
+++ Q1,2,3.js
@@ -3,10 +3,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("DONE");
 // Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
-let name = "ABdul Rafay"; // This saves the name
+let name = "Junaid Ul Haque Sheikh"; // This saves the name
 console.log(`Hello ${name}, would you like to learn some TypeScript today?`); // This shows the message
 // Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-let myNAme = `Abdul Rafay`;
+let myNAme = `Junaid Ul Haque Sheikh`;
 console.log(myNAme.toLowerCase());
 console.log(myNAme.toUpperCase());
 console.log(myNAme.charAt(0).toUpperCase() + myNAme.slice(1).toLowerCase());
